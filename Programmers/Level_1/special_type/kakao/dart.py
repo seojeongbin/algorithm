37136d9c8b7cbb997208425989c0ad733e0e26cd
seojeongbin <69031537+seojeongbin@users.noreply.github.com>
2022-07-10 22:45:40 +0900

    commit

--- Programmers/Level_1/special_type/kakao/dart.py
+++ Programmers/Level_1/special_type/kakao/dart.py
@@ -12,7 +12,44 @@ single / double / triple 영역에따라 **n
 - S, D, T 영역은 꼭 하나씩은 존재함
 ex) 
 1S2D*3T => 1S, 2D*, 3T
-1S*2T*3S => 1S*, 2T*, 3S => 4 + 16 + 3
-<< 태블릿에 유형별로 그려보고 어떻게 할지 코드짜기 >>  	
+1S*2T*3S => 1S*, 2T*, 3S => 4 + 16 + 3	
 '''
 
+def solution(string) :
+
+    string = string.replace('10','!')
+    # 10을 처리해주기 위해!!!! << 이 부분만 빼고 tc 모두 통과. 이 부분만 구글링으로 아이디어 얻음.
+    # 형태때문에 특수하게 처리하기 어려운 경우 형태를 원하는대로 바꿔줄 수 있음! 
+    check_type = ['S', 'D', 'T']
+    check_symbol = ['*', '#']
+    check_int = ['!']
+    for i in range(11) :
+        check_int.append(str(i))
+
+    score_box = [0] * 3
+    idx_pointer = -1 # 0부터 시작하도록 맞춰주기 위해
+    for elem in string :
+        if elem in check_int :
+            idx_pointer += 1
+            if elem == '!' : elem = 10
+            score_box[idx_pointer] = int(elem)
+        if elem in check_type :
+            if elem == 'S' : score_box[idx_pointer] **= 1
+            if elem == 'D' : score_box[idx_pointer] **= 2
+            if elem == 'T' : score_box[idx_pointer] **= 3
+        if elem in check_symbol :
+            if elem == '#' : score_box[idx_pointer] *= (-1)
+            if elem == '*' :
+                score_box[idx_pointer] *= 2
+                if idx_pointer > 0 :
+                    score_box[idx_pointer-1] *= 2
+        
+    result = sum(score_box)
+    return result
+
+if __name__ == '__main__' :
+    
+    dart_strings = ['1S2D*3T', '1S*2T*3S', '1D#2S*3S' ,'1D2S#10S'] 
+    for dart_string in dart_strings :
+        result = solution(dart_string)
+        print(result)
