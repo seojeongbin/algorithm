6672776486a7c71c9f9da2eb9be674064661433b
seojeongbin <69031537+seojeongbin@users.noreply.github.com>
2022-07-14 16:16:45 +0900

    .

--- Programmers/Level_1/special_type/brute_force/minimum_rectangle.py
+++ Programmers/Level_1/special_type/brute_force/minimum_rectangle.py
@@ -60,7 +60,7 @@ def solution(card_list) :
                 result_new = w_max * max(height_new)
                 if result_new < result :
                     result = result_new
-                    print(f'w_max : {w_max}, max(width_new) : {max(height_new)}') 
+                    # print(f'w_max : {w_max}, height_new : {height_new}, max(width_new) : {max(height_new)}') 
     else :
         for idx, card in enumerate(card_list) :
             if idx != h_max_idx :
@@ -69,7 +69,7 @@ def solution(card_list) :
                 result_new = h_max * max(width_new)
                 if result_new < result :
                     result = result_new
-                    print(f'h_max : {h_max}, max(width_new) : {max(width_new)}')       
+                    # print(f'h_max : {h_max}, width_new : {width_new},, max(width_new) : {max(width_new)}')       
     return result
 
 
@@ -83,3 +83,13 @@ if __name__ == '__main__' :
     sizes = [[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]
     result = solution(sizes)
     print(f'result : {result}')
+
+'''
+일부 tc만 통과하고 틀림.
+=> 가로, 세로 중 큰 값을 모두 가로로 두고, 작은값은 세로로 취급
+=> 가로/세로 가장 큰 값으로 만든 사각형의 넓이가 답이 됨.
+'''
+
+def solution(sizes):
+    # https://leeiopd.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-level1-%EC%B5%9C%EC%86%8C%EC%A7%81%EC%82%AC%EA%B0%81%ED%98%95-Python-%ED%8C%8C%EC%9D%B4%EC%8D%AC?category=944401?category=944401
+    pass
---
+++ Programmers/Level_1/special_type/kakao/failure_rate.py
@@ -0,0 +1,54 @@
+'''
+level : 1
+type : 2019 KAKAO BLIND RECRUITMENT
+title : failure rate
+refer : https://school.programmers.co.kr/learn/courses/30/lessons/42889
+description :
+- N = 스테이지 수
+- stages = 멈춰있는 스테이지 번호
+- result = 실패율이 높은 스테이지 내림차순 (머문사람 / 도달한사람)
+flow
+1. 스테이지 별 도달한사람과 머물고 있는 사람을 딕셔너리
+2. 스테이지 별 실패율을 딕셔너리로 저장
+3. 실패율(value)을 내림차순으로 갖는 스테이지 번호(key)를 리스트로 출력 
+'''
+
+'''
+1. 딕셔너리 활용 가능성을 항상 이제 염두하기
+2. 나눌때는 0 주의 & 런타임에러 뜰경우 0으로 나누진 않았는가 체크
+3. 빈 리스트에 append 할때는 (리스트 한줄표현!!) & lamgda 사용법 숙지하기
+'''
+
+def solution(n, stages) :
+    # 1. 스테이지 n개 별로 도달한 사람을 구한다
+    dict = {}
+    for i in range(n) :
+        i += 1
+        stage_now = stages.count(i)
+        # 2. stages 리스트에서 i 이상인 수를 카운트 
+        # count = sum(map(lambda x : x >= i, stages)) => 시간초과
+        count = len([x for x in stages if x>=i]) # 이렇게 lambda 사용하지않고 한줄로도 표현가능..
+        if count == 0 :
+            dict[i] = 0
+        else :
+            dict[i] = stage_now / count
+    # 3. 실패율(value) 기준 내림차순
+    sorted_dict = sorted(dict.items(), key = lambda item: item[1], reverse = True)
+    # 4. key를 리스트로 출력
+    result = []
+    for i in range(n) :
+        result.append(sorted_dict[i][0])
+    return result
+
+
+if __name__ == '__main__' :
+    
+    n = 5
+    stages = [2, 1, 2, 6, 2, 4, 3, 3]	
+    result = solution(n, stages)
+    print(f'result : {result}\n')
+    n = 4
+    stages = [4, 4, 4, 4, 4]	
+    result = solution(n, stages)    
+    print(f'result : {result}\n')
+
